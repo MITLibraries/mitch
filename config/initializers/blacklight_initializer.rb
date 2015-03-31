0093bff15d16ec4a9e9813ad017e95e73dc130bd
Jeremy Prevost <jprevost@mit.edu>
2015-03-31 09:31:29 -0400

    Initial Install
    Application generated via:
    `$ rails new app-name -m
    https://raw.githubusercontent.com/sul-dlss/spotlight/master/template.rb
    `

---
+++ config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+# Blacklight.secret_key = '45f5f041ac6ea4fa2901c2d55073a54a77737168ef451dc52ff120b80f002d5f63050c07bf1f8bf50b433e5bd86db0747e38f96e8eb764eeb73aa7b3f2a5b753'
+
